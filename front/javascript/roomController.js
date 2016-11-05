ac958fe931596e1f1132089536174c697d61f985
Brady Boettcher <bradyboettcher@gmail.com>
2016-11-05 18:46:42 -0500

    Hardcoded chat messages
    json hardcoded in, next is getting AWS up

--- front/javascript/roomController.js
+++ front/javascript/roomController.js
@@ -1,16 +1,15 @@
 app.controller('RoomController', function($scope, $http, $rootScope, $location) {
 
-//    var socket = io.connect();
+//    var socket = io.connect(); 
     
-    function getTest(){
-//         $http.get("/api/test").success(function(data){
-//            $scope.getResults = data;
-//        });
-    }   
+    
     var init = function () {
-        $http.get("/api/rooms").success(function(data){
-            console.log(data[0]);
-        });
+        /*$http.get("/api/rooms").success(function(data){
+            $scope.roomData = data[0];
+            console.log(roomData);
+                
+        });*/
+        $scope.roomData = {"_id":"581e517994e0802cf333c7b2","links":[],"files":[],"pictures":[],"chats":[{"message":"Hey Petr!","from":"bbottecher","time":3},{"message":"you there?","from":"bboettcher","time":2}],"people":["bboettcher","petr"]};
     };
     
    $scope.scopeTest = "ScopeTest"    
