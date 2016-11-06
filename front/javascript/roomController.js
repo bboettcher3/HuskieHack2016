563df08ec7facc51f0c22aceca7729f275df968b
Brady Boettcher <bradyboettcher@gmail.com>
2016-11-05 19:40:16 -0500

    Added submit button
    submit button works

--- front/javascript/roomController.js
+++ front/javascript/roomController.js
@@ -1,14 +1,18 @@
 app.controller('RoomController', function($scope, $http, $rootScope, $location) {
 
 //    var socket = io.connect(); 
-    
+    $scope.sendMessage = function() {
+        var message = document.getElementById("textBoxChat").value;
+        document.getElementById("textBoxChat").value = "";
+        console.log(message);
+    }
     
     var init = function () {
-        /*$http.get("/api/rooms").success(function(data){
+        $http.get("/api/rooms").success(function(data){
             $scope.roomData = data[0];
-            console.log(roomData);
+            console.log($scope.roomData);
                 
-        });*/
+        });
         $scope.roomData = {"_id":"581e517994e0802cf333c7b2","links":[],"files":[],"pictures":[],"chats":[{"message":"Hey Petr!","from":"bbottecher","time":3},{"message":"you there?","from":"bboettcher","time":2}],"people":["bboettcher","petr"]};
     };
     
