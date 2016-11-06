228968b084ab2841b530e5ef5e5556fd2e433197
marisatisler <tisler@wisc.edu>
2016-11-05 23:19:16 -0500

    fixed start

--- front/javascript/startController.js
+++ front/javascript/startController.js
@@ -16,16 +16,46 @@ app.controller('StartController', function($scope, $http, $rootScope, $location)
     
     
     function parseRoomName(room, username) {
-        /*console.log(room[0].people);        
-        console.log(username);*/
         
-        for(i = 0; i < room.length; i++) { 
-            var check = room[i].people;
-            if(check !== username) {
-                room[i].roommates += check + " ";
+        room.forEach(function(element){
+            element.people.forEach(function(elementPeople){
+                if (elementPeople != username) {    
+                    if (!element.roommates) {
+                        //need to define roomates
+                        element.roommates = elementPeople;
+                    } else {
+                        element.roommates += ", " + elementPeople;
+                    }
+                }
+            })
+            
+        })
+        
+        /*for(i = 0; i < room.length; i++) {
+            break;
+            console.log("room loop " + i);
+            var roomVec = room[i].people; //grabs people array
+            console.log(roomVec);
+            for(j = 0; j < roomVec.length; j++) {
+                            console.log("peo loop " + j);
+                            console.log(roomVec[j]);
+
+                if(roomVect[j] != username) {
+                    if(room[i].roommates) {
+                        
+                    
+                    room[i].roommates += roomVect[j] + " ";
+                    } else {
+                        room[i].roommates = roomVect[j] + " ";
+                    }
+                }
             }
-            }
-        }   
+        } console.log(room);*/
+    }
+        
+        
+                //room[i].roommates += check + " ";
+            
     
         
     var init = function (scope) {
