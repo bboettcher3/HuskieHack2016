e8f69814d7e625262591f5e2f63f12702095efbd
marisatisler <tisler@wisc.edu>
2016-11-05 22:23:03 -0500

    working on start

--- front/javascript/startController.js
+++ front/javascript/startController.js
@@ -7,19 +7,37 @@ app.controller('StartController', function($scope, $http, $rootScope, $location)
         console.log(user);
         $http.get("/api/rooms/byUser/"+user.username).success(function(data){
             console.log(data);
-            $scope.rooms = data;
+            $scope.rooms = data; //data == array 
+            parseRoomName(data, $scope.pickedUser.username);
+            
+            
         });
     }
     
-    var init = function () {
+    
+    function parseRoomName(room, username) {
+        /*console.log(room[0].people);        
+        console.log(username);*/
+        
+        for(i = 0; i < room.length; i++) { 
+            var check = room[i].people;
+            if(check !== username) {
+                room[i].roommates += check + " ";
+            }
+            }
+        }   
+    
+        
+    var init = function (scope) {
         $http.get("/api/users").success(function(data){
             console.log(data);
+        
             $scope.users = data;
         });
     };
     
    $scope.scopeTest = "ScopeTest"    
   
-    
+    //$scope.pickedUser.username == petr
 init();
 });
